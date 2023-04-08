90abcb55ba2da9b431f86a9f3021c335e085870e
Mukesh Gadupudi <shanmu9898@gmail.com>
2023-04-08 13:44:48 -0400

    Changed parameters and used paginator

--- size_calculation.py
+++ size_calculation.py
@@ -2,46 +2,53 @@ import datetime
 import boto3
 import pytz
 import re
+import argparse
 
 def get_size(bucket, prefix, date, regex):
     s3 = boto3.client('s3')
     s3_resource = boto3.resource('s3')
 
     # Convert date string to datetime object
-    date_obj = datetime.datetime.strptime(date, '%Y-%m-%d')
+    start_date = None
+    if date:
+        date_obj = datetime.datetime.strptime(date, '%Y-%m-%d')
 
-    # Make start and end dates timezone-aware
-    timezone = pytz.timezone('UTC')
-    start_date = timezone.localize(datetime.datetime.combine(date_obj, datetime.time.min)).isoformat()
-    end_date = timezone.localize(datetime.datetime.combine(date_obj, datetime.time.max)).isoformat()
+        # Make start and end dates timezone-aware
+        timezone = pytz.timezone('UTC')
+        start_date = timezone.localize(datetime.datetime.combine(date_obj, datetime.time.min)).isoformat()
 
-    # Get all objects in the specified prefix and date range
-    objects = s3.list_objects_v2(Bucket=bucket, Prefix=prefix, MaxKeys=1000, StartAfter=start_date)
+    paginator = s3.get_paginator('list_objects')
+    objects = paginator.paginate(Bucket=bucket, Prefix=prefix)
 
     # Calculate the total size of all objects
     total_size = 0
-    for obj in objects['Contents']:
-        if obj['LastModified'].strftime('%Y-%m-%d') == date and re.match(regex, obj['Key']):
-            print(f"File being added {obj['Key']}")
-            total_size += obj['Size']
+    count = 0
+    for obj in objects:
+        for content in obj.get('Contents', []):
+            last_modified = content.get('LastModified')
+            if start_date and not str(last_modified).startswith(date):
+                continue
+            if regex and not re.search(regex, content.get('Key')):
+                continue
+            total_size += content.get('Size')
+            count += 1
 
-    return total_size
+    return total_size, count
 
 
 def main():
-    bucket_name = 'mktestvimo'
-    # Prompt user for folder_name if not set
-    folder_name = input('Enter folder name: ') or 'Test'
-    print(f"Setting folder_name {folder_name}")
-    # Prompt user for date if not set
-    date = input('Enter date in YYYY-MM-DD format: ') or '2016-05-06'
-    print(f"Setting date {date}")
-    # Prompt user for regex if not set
-    regex = input('Enter regular expression to filter files: ') or r'.*\.json'
-    print(f"Setting regex {folder_name}")
-    total_size = get_size(bucket_name, folder_name, date, regex)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--bucket", default="my-bucket-name", help="S3 bucket name")
+    parser.add_argument("--folder", required=True, help="Folder name inside S3 bucket")
+    parser.add_argument("--date", help="Last modified date of the files in format 'YYYY-MM-DD'")
+    parser.add_argument("--regex", default="", help="Regex to filter for specific files")
+    args = parser.parse_args()
 
-    print(f'Total file size: {total_size} bytes')
+    regex_string = f".*{args.regex}.*"
+    regex = re.compile(regex_string)
+
+    total_size, count = get_size(args.bucket, args.folder, args.date, regex)
+    print(f'Total file size: {total_size / (1024*1024*1024):.6f} GB in {count} files')
 
 if __name__ == '__main__':
     main()
